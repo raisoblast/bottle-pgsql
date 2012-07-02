53be8e6641c3a8e161c0bbd1b899464d333de1a8
Arif Kurniawan <arifk97@gmail.com>
2012-07-02 17:15:36 +0700

    first commit, version 0.1.0

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from distutils.core import setup
+
+try:
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:
+    from distutils.command.build_py import build_py
+
+# This ugly hack executes the first few lines of the module file to look up some
+# common variables. We cannot just import the module because it depends on other
+# modules that might not be installed yet.
+filename = os.path.join(os.path.dirname(__file__), 'bottle_pgsql.py')
+source = open(filename).read().split('### CUT HERE')[0]
+exec(source)
+
+setup(
+    name = 'bottle-pgsql',
+    version = __version__,
+    url = 'https://github.com/raisoblast/bottle-pgsql',
+    description = 'PgSQL integration for Bottle',
+    long_description = __doc__,
+    author = 'Arif Kurniawan',
+    author_email = 'arifk97@gmail.com',
+    license = __license__,
+    platforms = 'any',
+    py_modules = [
+        'bottle_pgsql'
+    ],
+    requires = [
+        'bottle (>=0.10)'
+    ],
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    cmdclass = {'build_py': build_py}
+)
